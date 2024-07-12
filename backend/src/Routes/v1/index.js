51b2df22a426c2b3353de50c062714c831ac948d
AvhikBiswas <abhikbiswas351@gmail.com>
2024-07-12 20:29:37 +0530

    refac: routes added

--- backend/src/Routes/v1/index.js
+++ backend/src/Routes/v1/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import toggleDishes from "../../Controller/toggleDishes.js";
+import allDishes from "../../Controller/allDishes.js";
 
 const router = express.Router();
 
